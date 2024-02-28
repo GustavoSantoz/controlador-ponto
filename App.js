c3bb01879fddfa3566b27de250cea1275c682434
Gustavo dos Santos Nascimento <74427958+GustavoSantoz@users.noreply.github.com>
2024-02-28 02:40:23 +0000

    add expo cell

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+      style={styles.input}
+      />
+      <Text style={styles.total}>O total é:</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#74b816',
+    fontSize: '5em',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  total: {
+    fontSize: 30,
+  },
+
+  input: {
+    height: 30,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
